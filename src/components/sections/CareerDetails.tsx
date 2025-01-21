32a13171313d42d3c3dc304dd732f49f4f117b0b
Daniel Fang <danielfang@hotmail.com>
2025-01-21 15:20:28 -0800

    fix build issues

--- src/components/sections/CareerDetails.tsx
+++ src/components/sections/CareerDetails.tsx
@@ -1,27 +1,25 @@
 import { CAREER_DESCRIPTIONS } from "../../constants/descriptions";
-import TypeWriter from "../TypeWriter";
 import "./CareerDetails.css";
 
 interface CareerDetailsProps {
   roleId: string;
 }
 
+// Define the type for career descriptions
+type CareerDescriptionType = {
+  [key in "arista" | "arista-intern" | "sap-intern"]: string[];
+};
+
 const CareerDetails = ({ roleId }: CareerDetailsProps) => {
-  const details = CAREER_DESCRIPTIONS[roleId];
+  const details = CAREER_DESCRIPTIONS[roleId as keyof CareerDescriptionType];
 
   if (!details) return null;
 
   return (
     <div className='career-details'>
       <ul>
-        {details.map((point, index) => (
-          <li key={index}>
-            <TypeWriter
-              text={point}
-              speed={0.5}
-              delay={index * 0.2} // Stagger the start of each line
-            />
-          </li>
+        {details.map((point: string, index: number) => (
+          <li key={index}>{point}</li>
         ))}
       </ul>
     </div>
