f0a35196b30eb523f488d870d24446ed3a52e3b1
Daniel Fang <33407178+swFang@users.noreply.github.com>
2026-02-25 16:05:29 -0800

    update siteup privacy policy

--- src/constants/apps.ts
+++ src/constants/apps.ts
@@ -4,7 +4,7 @@ export const APP_MARKDOWN_CONTENT: Record<AppSectionId, string> = {
   siteup: `
 **SiteUp Privacy Policy**
 
-This privacy policy applies to the testname app (hereby referred to as "Application") for mobile devices that was created by testdeveloper (hereby referred to as "Service Provider") as a Freemium service. This service is intended for use "AS IS".
+This privacy policy applies to the SiteUp app (hereby referred to as "Application") for mobile devices that was created by Shi-Wei Fang (hereby referred to as "Service Provider") as a Freemium service. This service is intended for use "AS IS".
 
 **What information does the Application obtain and how is it used?**
 
@@ -26,7 +26,7 @@ You can stop all collection of information by the Application easily by uninstal
 
 The Application is not used to knowingly solicit data from or market to children under the age of 13.
 
-The Service Provider does not knowingly collect personally identifiable information from children. The Service Provider encourages all children to never submit any personally identifiable information through the Application and/or Services. The Service Provider encourage parents and legal guardians to monitor their children's Internet usage and to help enforce this Policy by instructing their children never to provide personally identifiable information through the Application and/or Services without their permission. If you have reason to believe that a child has provided personally identifiable information to the Service Provider through the Application and/or Services, please contact the Service Provider (test@gmail.com) so that they will be able to take the necessary actions. You must also be at least 16 years of age to consent to the processing of your personally identifiable information in your country (in some countries we may allow your parent or guardian to do so on your behalf).
+The Service Provider does not knowingly collect personally identifiable information from children. The Service Provider encourages all children to never submit any personally identifiable information through the Application and/or Services. The Service Provider encourage parents and legal guardians to monitor their children's Internet usage and to help enforce this Policy by instructing their children never to provide personally identifiable information through the Application and/or Services without their permission. If you have reason to believe that a child has provided personally identifiable information to the Service Provider through the Application and/or Services, please contact the Service Provider (danielfang.work@gmail.com) so that they will be able to take the necessary actions. You must also be at least 16 years of age to consent to the processing of your personally identifiable information in your country (in some countries we may allow your parent or guardian to do so on your behalf).
 
 **Security**
 
@@ -44,6 +44,6 @@ By using the Application, you are consenting to the processing of your informati
 
 **Contact Us**
 
-If you have any questions regarding privacy while using the Application, or have questions about the practices, please contact the Service Provider via email at test@gmail.com.
+If you have any questions regarding privacy while using the Application, or have questions about the practices, please contact the Service Provider via email at danielfang.work@gmail.com.
 `,
 };
